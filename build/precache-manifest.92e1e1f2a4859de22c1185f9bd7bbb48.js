de35d2ece8a6f59dd227a6be3df855cd7376e572
khalil <khalil.hamze@cloudgate.consulting>
2019-12-02 11:32:51 +0200

    master: Host the compugear site on netlify

---
+++ build/precache-manifest.92e1e1f2a4859de22c1185f9bd7bbb48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2487cdedef6cf85bb8ab1bf0137280a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a275b05542f479cd4571",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "79dd8d5834fa849528fa",
+    "url": "/static/css/main.ecd3c275.chunk.css"
+  },
+  {
+    "revision": "a275b05542f479cd4571",
+    "url": "/static/js/2.2ea6d167.chunk.js"
+  },
+  {
+    "revision": "79dd8d5834fa849528fa",
+    "url": "/static/js/main.56e9d83c.chunk.js"
+  },
+  {
+    "revision": "4dee98ff7cefab17b71e",
+    "url": "/static/js/runtime-main.fa2f4f29.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
