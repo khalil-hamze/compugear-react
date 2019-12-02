05680fd4dc4f099ba1c137c1d8501a317a2220b2
khalil <khalil.hamze@cloudgate.consulting>
2019-12-02 12:01:00 +0200

    master: Fix className instead class

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -116,18 +116,18 @@ const particleOpt = {
 function Jumbotron() {
   return (
     <section id="home">
-      <div class="landing">
-        <div class="home-wrap">
+      <div className="landing">
+        <div className="home-wrap">
           <div id="particles-js">
             <Particles params={particleOpt} height="100vh" />
           </div>
-          <div class="caption text-center">
+          <div className="caption text-center">
             <span>Welcome to COMPUGEAR</span>
             <h3>Perform your Computer</h3>
-            <a class="btn btn-outline-light btn-lg" href="#">
+            <a className="btn btn-outline-light btn-lg" href="#">
               Who we are
             </a>
-            <a class="btn btn-outline-light btn-lg" href="#">
+            <a className="btn btn-outline-light btn-lg" href="#">
               products
             </a>
           </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,43 @@
 import React from "react";
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <section id="nav-bar">
-      <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-        <a class="navbar-brand" href="#"
-          ><img src="images/logo-compugear.png"
-        /></a>
+      <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <a className="navbar-brand" href="#">
+          <img src="images/logo-compugear.png" />
+        </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarResponsive"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#company">Company</a>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#company">
+                Company
+              </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#products&solutions"
-                >Products&Solutions</a
-              >
+            <li className="nav-item">
+              <a className="nav-link" href="#products&solutions">
+                Products&Solutions
+              </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#ourcustomers">Customers</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#ourcustomers">
+                Customers
+              </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#contact">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
             </li>
           </ul>
         </div>
