351002b2b352fe6856da7332e5213a0280b49056
Khalil.h1 <khalil-hamze@hotmail.com>
2019-12-16 22:48:21 +0200

    master: Fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./About.css"
 
 function About() {
   return (
     <section id="About Us">
       <h1>About Us</h1>
       <p>
-        Founded in 2013, CompuGear offer quality IT solutions for home and
+        Founded in 2013, Compugnear offer quality IT solutions for home and
         business users at a price that everyone can afford. Our customers will
         always receive one-on-one personal attention at a very affordable price.
         Our customers will receive the highest quality of customer service
