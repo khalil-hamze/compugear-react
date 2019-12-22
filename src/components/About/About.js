5388344829bf7096aa8dbc1567c956d749183548
Khalil.h1 <khalil-hamze@hotmail.com>
2019-12-22 21:39:16 +0200

    master: add about-us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,24 @@
 import React from "react";
-import "./About.css"
+import "./About.css";
 
 function About() {
   return (
-    <section id="About Us">
-      <h1>About Us</h1>
-      <p>
-        Founded in 2013, Compugnear offer quality IT solutions for home and
-        business users at a price that everyone can afford. Our customers will
-        always receive one-on-one personal attention at a very affordable price.
-        Our customers will receive the highest quality of customer service
-        available.
-      </p>
+    <section id="about-us">
+      <div className="container">
+        <div className="title">
+          <h1 className="font-weight-bold">About Us</h1>
+        </div>
+        <div className="about-content">
+          <h3 className='title-content'>Who We Are</h3>
+          <p align='justify'>
+            Founded in 2013, Compugnear offer quality IT solutions for home and
+            business users at a price that everyone can afford. Our customers
+            will always receive one-on-one personal attention at a very
+            affordable price. Our customers will receive the highest quality of
+            customer service available.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
